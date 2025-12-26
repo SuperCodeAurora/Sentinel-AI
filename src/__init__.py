8e42d84fc8c658fbcc5c759f92351d1ceac662b0
SuperCodeAurora <charles.fang.huang@gmail.com>
2025-12-26 11:38:42 +0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,6 +1,7 @@
-from .core_firewall import AegisFirewall
+# Expose the main class so users can import DiamondShield
+from .core_firewall import DiamondShield
 
 __version__ = "1.0.0"
-__author__ = "Cyber Security Department"
+__author__ = "SuperCodeAurora"
 
-__all__ = ["AegisFirewall"]
+__all__ = ["DiamondShield"]
